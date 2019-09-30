3cea6a73b63660c008a239b45f6ab516324ce2e3
cyhbyw <cyhbyw13914855>
2019-09-30 17:15:58 +0800

    《Netty权威指南第2版》源码：降了netty版本，修改了一点代码

---
+++ NettyDefinitiveGuide_2ndEdition/chapter04_tcpFrame/src/main/java/com/cyh/correct/TimeServerHandler.java
@@ -0,0 +1,32 @@
+package com.cyh.correct;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author lilinfeng
+ * @date 2014年2月14日
+ * @version 1.0
+ */
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+
+    private int counter;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        String body = (String) msg;
+        System.out.println("The time server receive order : " + body + " ; the counter is : " + ++counter);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body)
+                ? new java.util.Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+        currentTime = currentTime + System.getProperty("line.separator");
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.writeAndFlush(resp);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        ctx.close();
+    }
+}
---
+++ NettyDefinitiveGuide_2ndEdition/chapter05_delimiterAndFixLength/src/main/java/com/cyh/fixedLen/EchoServerHandler.java
@@ -0,0 +1,25 @@
+package com.cyh.fixedLen;
+
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author lilinfeng
+ * @date 2014年2月14日
+ * @version 1.0
+ */
+@Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        System.out.println("Receive message from client: [" + msg + "]");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();// 发生异常，关闭链路
+    }
+}
